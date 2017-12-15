be7a91d454f56139307b2e4cd559667b6be74126
Wiktor <wiktor.boniecki@gmail.com>
2017-12-15 22:18:19 +0100

    fix static

--- web_tim/settings.py
+++ web_tim/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'web_tim.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,12 +112,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-ENV_PATH = os.path.abspath(os.path.dirname(__file__))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(ENV_PATH, 'templates/static/'),
+    os.path.join(BASE_DIR, 'templates/static/'),
 )
 
 API = 'paste_api_link'
